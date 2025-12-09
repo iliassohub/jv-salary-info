43da8552b190e5d2c8b9048a2ab32609284c9d63
Ilias <iliasaiakos@gmail.com>
2025-12-09 20:01:43 +0100

    first try

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,8 +4,14 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_CHECK = 4;
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int RATE_INDEX = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         LocalDate fromDate = LocalDate.parse(dateFrom, formatter);
         LocalDate toDate = LocalDate.parse(dateTo, formatter);
         StringBuilder header = new StringBuilder();
@@ -14,17 +20,17 @@ public class SalaryInfo {
         for (String dataLine : data) {
 
             String[] splitData = dataLine.split(" ");
-            if (splitData.length >= 4) {
-                String dateToken = splitData[0];
-                String nameToken = splitData[1];
-                int hoursToken = Integer.parseInt(splitData[2]);
-                int rateToken = Integer.parseInt(splitData[3]);
+            if (splitData.length >= DATE_CHECK) {
+                String dateToken = splitData[DATE_INDEX];
+                String nameToken = splitData[NAME_INDEX];
+                int hoursToken = Integer.parseInt(splitData[HOURS_INDEX]);
+                int rateToken = Integer.parseInt(splitData[RATE_INDEX]);
                 int daySalary = hoursToken * rateToken;
 
                 LocalDate properDateToken = LocalDate.parse(dateToken, formatter);
 
-                for (int i = 0; i < names.length; i++) {
-                    if (!properDateToken.isBefore(fromDate) && !properDateToken.isAfter(toDate)) {
+                if (!properDateToken.isBefore(fromDate) && !properDateToken.isAfter(toDate)) {
+                    for (int i = 0; i < names.length; i++) {
                         if (nameToken.equals(names[i])) {
                             totals[i] += daySalary;
                         }
